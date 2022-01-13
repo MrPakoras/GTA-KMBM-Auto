b830fd1e9dcb77a6e55f2c054b9e1d3a683b2a28
MrPakoras <hartanas8g@gmail.com>
2022-01-13 12:10:07 +0000

    Update KMBM Auto v0.4.py

--- KMBM Auto v0.4.py
+++ KMBM Auto v0.4.py
@@ -1,4 +1,8 @@
 # Kiddions Mod Bunker Mission Automater
+# Inspired by: https://www.unknowncheats.me/forum/grand-theft-auto-v/329379-automating-kiddions-modest-menu-minimum-job-payout-blow-ii.html
+
+### IMPORTANT - READ THE README!! :D
+
 
 import pyautogui, time, ctypes, math, pywinauto
 import pydirectinput as pdi
